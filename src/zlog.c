056f107e6ecb495e0eb6d4fb12193782ff9996dd
Marc Schier <marcschier@hotmail.com>
2016-10-06 17:44:15 +0200

    Ensure thread is only deleted once

--- src/zlog.c
+++ src/zlog.c
@@ -52,7 +52,7 @@ static void zlog_clean_rest_thread(void)
 	zlog_thread_t *a_thread;
 	a_thread = zc_tls_get(zlog_thread_key);
 	if (a_thread) zlog_thread_del(a_thread);
-    zc_tls_destroy(zlog_thread_key);
+    zc_tls_set(zlog_thread_key, NULL);
 	return;
 }
 
